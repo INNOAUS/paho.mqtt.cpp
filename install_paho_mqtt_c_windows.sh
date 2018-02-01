9d921b946d783f01ec0c45d5eb1f3b1a9cb1e431
Leo An <leo@innoaus.com>
2018-02-01 13:14:15 +0900

    .

--- install_paho_mqtt_c_windows.sh
+++ install_paho_mqtt_c_windows.sh
@@ -3,12 +3,17 @@
 # Installs the matching version of Paho MQTT C library required by the C++ lib.
 #
 
-git clone https://github.com/eclipse/paho.mqtt.c.git
+#git clone https://github.com/eclipse/paho.mqtt.c.git /d/paho.mqtt.c
 #git checkout develop
-cd paho.mqtt.c && mkdir build.windows.x64 && cd build.windows.x64
-cmake -G "Visual Studio 15 2017 Win64" -DBUILD_STATIC_LIBS=TRUE -DBUILD_SHARED_LIBRARIES=FALSE ..
-cd .. && mkdir build.windows.x86 && cd build.windows.x86
-cmake -G "Visual Studio 15 2017" -DBUILD_STATIC_LIBS=TRUE -DBUILD_SHARED_LIBRARIES=FALSE ..
+mkdir build.vs2017.x64 && cd build.vs2017.x64
+cmake -G "Visual Studio 15 2017 Win64" -DPAHO_WITH_SSL=OFF -DBUILD_STATIC_LIBS=TRUE -DBUILD_SHARED_LIBRARIES=FALSE ..
+#cmake --build . --target ALL_BUILD --config Release
+#cmake --build . --target ALL_BUILD --config Debug
+
+cd .. && mkdir build.vs2017.x86 && cd build.vs2017.x86
+cmake -G "Visual Studio 15 2017" -DPAHO_WITH_SSL=OFF -DBUILD_STATIC_LIBS=TRUE -DBUILD_SHARED_LIBRARIES=FALSE ..
+#cmake --build . --target ALL_BUILD --config Release
+#cmake --build . --target ALL_BUILD --config Debug
 
 exit 0
 
